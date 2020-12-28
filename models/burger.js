05aadf3f4111c12287e5d5cb6716e44fa2d16e99
Jackson Smith <jsmithbootcamp2020@gmail.com>
2020-12-27 22:16:19 -0500

    working through code for model and controller

---
+++ models/burger.js
@@ -0,0 +1,28 @@
+// Import the ORM//
+const orm = require('../config/orm.js');
+
+const burgers = {
+  all(cb) {
+    orm.all('burgers', function(res) {
+      cb(res);
+    });
+  },
+  create(cols, vals, cb) {
+    orm.create('burgers', cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update(objColVals, condition, cb) {
+    orm.update('burgers', objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete(condition, cb) {
+    orm.delete('burgers', condition, function(res) {
+      cb(res);
+    });
+  },
+};
+
+// Export the DB functions for controller (burgers_controller.js).
+module.exports = burgers;
